be8b5fed1b307fdf51beb6278e8222d7a3c3a375
wd <wd@wdicc.com>
2010-07-25 22:29:23 +0800

    add two shortcut for quick jump

--- site-lisp/config/wd-usefull-func.el
+++ site-lisp/config/wd-usefull-func.el
@@ -126,3 +126,26 @@
         (t (self-insert-command (or arg 1)))))
 
 (provide 'wd-usefull-func)
+
+;;
+;; 使用 C-. 标记， C-, 切换
+;;
+
+(global-set-key [(control ?\.)] 'ska-point-to-register)
+(global-set-key [(control ?\,)] 'ska-jump-to-register)
+(defun ska-point-to-register()
+  "Store cursorposition _fast_ in a register.
+Use ska-jump-to-register to jump back to the stored
+position."
+  (interactive)
+  (setq zmacs-region-stays t)
+  (point-to-register 8))
+
+(defun ska-jump-to-register()
+  "Switches between current cursorposition and position
+that was stored with ska-point-to-register."
+  (interactive)
+  (setq zmacs-region-stays t)
+  (let ((tmp (point-marker)))
+    (jump-to-register 8)
+    (set-register 8 tmp)))
