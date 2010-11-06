ca10b9632ec1b9b413ec06252753539e5ad8a5d7
wd <wd@wdicc.com>
2010-11-06 11:22:26 +0800

    add some fonts settings

--- site-lisp/config/wd-misc.el
+++ site-lisp/config/wd-misc.el
@@ -4,8 +4,102 @@
 
 (load "~/.pwds")
 
+;; (setq browse-url-browser-function 'browse-url-generic
+;;       browse-url-generic-program "chromium")
+
+
 (setq compilation-environment (list "LZ_HOST=t.api.linezing.com"))
 ;; (setq wd-make-env "LZ_HOST=t.api.linezing.com:9999 ")
+
+;; Way 2
+;; (let ((fontset (frame-parameter nil 'font))
+;;      (zh-font (font-spec :family "WenQuanYi Zen Hei" :size 14)))
+;;  (set-default-font "Monaco:pixelsize=10:foundry=unknown")
+;;  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;    (set-fontset-font fontset charset zh-font))
+;;  (add-to-list 'default-frame-alist `(font . ,fontset)))
+
+
+;; http://jff.googlecode.com/svn/trunk/XDE/xde/emacs/dot_emacs.d/site-start.d/01_font.el
+;; Way 1
+;(let ((zh-font "-unknown-AR PL UMing CN-*-*-*-*-16-*-*-*-*-*-*-*")
+(let ((zh-font "WenQuanYi Zen Hei:pixelsize=16")
+      (fontset "fontset-my"))
+  (create-fontset-from-fontset-spec
+    (concat
+      "-unknown-DejaVu Sans Mono-*-*-*-*-14-*-*-*-*-*-" fontset
+      ;; "-unknown-Monaco-*-*-*-*-15-*-*-*-*-*-" fontset
+      ",kana:"          zh-font
+      ",han:"           zh-font
+      ",symbol:"        zh-font
+      ",cjk-misc:"      zh-font
+      ",bopomofo:"      zh-font))
+  (set-default-font fontset)
+  (add-to-list 'default-frame-alist `(font . ,fontset)))
+  ;; or set font for new frame like this:
+  ;(add-to-list 'after-make-frame-functions
+  ;             (lambda (new-frame)
+  ;               (select-frame new-frame)
+  ;               (if window-system
+  ;                 (set-frame-font "fontset-my"))))
+
+
+
+
+;; 用来设置字体的
+;; (defun qiang-font-existsp (font)
+;;   (if (null (x-list-fonts font))
+;;       nil t))
+
+;; (defun qiang-make-font-string (font-name font-size)
+;;   (if (and (stringp font-size) 
+;;            (equal ":" (string (elt font-size 0))))
+;;       (format "%s%s" font-name font-size)
+;;     (format "%s %s" font-name font-size)))
+
+;; (defun qiang-set-font (english-fonts
+;;                        english-font-size
+;;                        chinese-fonts
+;;                        &optional chinese-font-size)
+;;   "english-font-size could be set to \":pixelsize=18\" or a integer.
+;; If set/leave chinese-font-size to nil, it will follow english-font-size"
+;;   (require 'cl)                         ; for find if
+;;   (let ((en-font (qiang-make-font-string
+;;                   (find-if #'qiang-font-existsp english-fonts)
+;;                   english-font-size))
+;;         (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts)
+;;                             :size chinese-font-size)))
+ 
+;;     ;; Set the default English font
+;;     ;; 
+;;     ;; The following 2 method cannot make the font settig work in new frames.
+;;     ;; (set-default-font "Consolas:pixelsize=18")
+;;     ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+;;     ;; We have to use set-face-attribute
+;;     (message "Set English Font to %s" en-font)
+;;     (set-face-attribute
+;;      'default nil :font en-font)
+ 
+;;     ;; Set Chinese font 
+;;     ;; Do not use 'unicode charset, it will cause the english font setting invalid
+;;     (message "Set Chinese Font to %s" zh-font)
+;;     (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;       (set-fontset-font (frame-parameter nil 'font)
+;;                         charset
+;;                         zh-font))))
+
+;; ;; 字体设置
+;; (qiang-set-font
+;;  '("Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") ":pixelsize=15"
+;;  '("WenQuanYi Zen Hei" "Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+
+;; (add-hook 'after-make-frame-functions
+;;   (lambda(arg)
+;;     (if  (&gt;= (length (frame-list)) 1)
+;;           (set-fontset-font  "fontset-startup" 'chinese-gbk (font-spec :family "WenQuanYi Zen Hei" :size 15) nil 'prepend)
+;;       )
+;;     ) t
+;; ))
 
 (require 'color-theme-hober2)
 (color-theme-hober2)
