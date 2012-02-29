de3a585206802463e71355ec2d272cbd898368ce
wd <wd@wdicc.com>
2012-02-29 16:19:01 +0800

    add some code to auto merge mobileorg.org

--- site-lisp/config/wd-org.el
+++ site-lisp/config/wd-org.el
@@ -167,7 +167,7 @@
 
 ;; ;; (defalias 'archive-done-tasks 'org-my-archive-done-tasks)
 
-(add-hook 'org-mode-hook 'soft-wrap-lines)
+;; (add-hook 'org-mode-hook 'soft-wrap-lines)
 
 
 (defun soft-wrap-lines ()
@@ -179,5 +179,44 @@ in current buffer."
   )
 
 (setq org-src-fontify-natively t)
+
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/todo.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+
+(defun my-org-convert-incoming-items ()
+  (interactive)
+  (with-current-buffer (find-file-noselect org-mobile-inbox-for-pull)
+    (goto-char (point-min))
+    (while (re-search-forward "^\\* " nil t)
+      (goto-char (match-beginning 0))
+      (insert ?*)
+      (forward-char 2)
+      (insert "TODO ")
+      (goto-char (line-beginning-position))
+      (forward-line)
+      (insert
+       (format
+        " SCHEDULED: %s
+:PROPERTIES:
+:ID: %s :END:
+"
+        (with-temp-buffer (org-insert-time-stamp (current-time)))
+        (shell-command-to-string "uuidgen"))))
+    (let ((tasks (buffer-string)))
+      (erase-buffer)
+      (save-buffer)
+      (kill-buffer (current-buffer))
+      (with-current-buffer (find-file-noselect "~/Dropbox/todo.org")
+        (save-excursion
+          (goto-char (point-min))
+          (search-forward "* Tasks")
+          (goto-char (match-beginning 0))
+          (insert tasks))))))
+ 
+(add-hook 'org-mobile-post-pull-hook 'my-org-convert-incoming-items)
 
 (provide 'wd-org)
