e6ac2fd0e7cba16d93e42427b131d1ed8179f1cf
wd <wd@wdicc.com>
2012-03-15 19:04:40 +0800

    remove gmail, cant connect

--- site-lisp/config/wd-gnus.el
+++ site-lisp/config/wd-gnus.el
@@ -2,10 +2,10 @@
 
 (setq gnus-startup-file "~/.emacs.d/.newsrc")
 
-(setq gnus-select-method '(nnimap "gmail"
-				  (nnimap-address "imap.gmail.com")
-				  (nnimap-server-port 993)
-				  (nnimap-stream ssl)))
+;; (setq gnus-select-method '(nnimap "gmail"
+;; 				  (nnimap-address "imap.gmail.com")
+;; 				  (nnimap-server-port 993)
+;; 				  (nnimap-stream ssl)))
 
 ;; (setq gnus-secondary-select-methods '((nntp "nntp.aioe.org")))
 (setq gnus-secondary-select-methods '((nntp "localhost")))
@@ -155,13 +155,13 @@
 
 ;; SMTP
 (setq message-send-mail-function 'smtpmail-send-it
+      send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "wd@wdicc.com" nil))
+      smtpmail-auth-credentials (expand-file-name "~/.authinfo")
       smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
-      smtpmail-local-domain "wdicc.com")
-
+      smtpmail-debug-info t)
 
 ;; ;; IMAP
 ;; (setq gnus-select-method
